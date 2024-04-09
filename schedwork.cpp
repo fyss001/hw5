b6efb45951b301458370111b711bf589579df843
Peiran <peiranqi@usc.edu>
2024-04-08 23:22:11 -0700

    Passed local tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,29 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool checkValid(const size_t, DailySchedule&, const size_t, const Worker_T);
 
+bool checkValid(const size_t maxShifts, DailySchedule& sched, const size_t day, const Worker_T id){
+    size_t cnt=0;
+    if(day!=0){
+        for(size_t i=0;i<day;i++){
+            for(size_t j=0;j<sched[i].size();j++){
+                if(sched[i][j]==id){
+                    cnt++;
+                    if(cnt>maxShifts-1){
+                        return false;
+                    }
+                    continue;
+                }
+            }
+        }
+    }
+    for(size_t j=0;j<sched[day].size();j++){
+        if(sched[day][j]==INVALID_ID) break;
+        if(sched[day][j]==id) return false;
+    }
+    return true;
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,11 +57,48 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
-    sched.clear();
-    // Add your code below
-
-
-
-
+    if(avail.size() == sched.size() && sched[sched.size()-1][dailyNeed-1]!=INVALID_ID){
+        return true;
+    }
+    vector<Worker_T> availToday;
+    vector<Worker_T> schedToday;
+    if(sched.empty()||sched[sched.size()-1][dailyNeed-1]!=INVALID_ID){
+        vector<Worker_T> temp;
+        temp.resize(dailyNeed);
+        std::fill(temp.begin(),temp.end(),INVALID_ID);
+        sched.push_back(temp);
+    }
+    size_t day=sched.size()-1;
+    schedToday=sched[day];    
+    for(size_t i=0;i<avail[day].size();i++){
+        if(avail[day][i]) availToday.push_back(i);
+    }
+    if(dailyNeed>availToday.size()){
+        if(sched[day][0]==INVALID_ID){
+            sched.pop_back();
+        }
+        return false;
+    }
+    for(size_t i=0;i<dailyNeed;i++){
+        if(schedToday[i]==INVALID_ID){
+            for(size_t j=0;j<availToday.size();j++){
+                if(checkValid(maxShifts, sched, day, availToday[j])){
+                    schedToday[i]=availToday[j];
+                    sched[day]=schedToday;
+                    if(schedule(avail, dailyNeed, maxShifts, sched)){
+                        return true;
+                    }else{
+                        schedToday[i]=INVALID_ID;
+                        sched[day]=schedToday;
+                    }
+                }
+            }
+            break;
+        }
+    }
+    if(sched[day][0]==INVALID_ID){
+        sched.pop_back();
+    }
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -22,7 +22,41 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    unsigned cnt=0;
+    string temp=in;
+    set<std::string> ans;
+    for(unsigned i=0;i<temp.length();i++){
+        if(temp[i]=='-'){
+            cnt++;
+        }
+    }
+    if(cnt<floating.length()) return ans;  //pruning
+    if(!cnt){
+        if(dict.count(in)&&floating==""){
+            ans.insert(in);
+        }
+        return ans;
+    }
+    for(unsigned i=0;i<temp.length();i++){
+        if(temp[i]=='-'){
+            for(int j=0;j<26;j++){
+                char c='a'+j;
+                string next="";
+                bool flag=1;
+                for(unsigned k=0;k<floating.length();k++){
+                    if(floating[k]!=c||!flag){
+                        next+=floating[k];
+                    }else{
+                        flag=0;
+                    }
+                }
+                temp[i]=c;
+                set<std::string> temp_set=wordle(temp, next, dict);
+                ans.insert(temp_set.begin(),temp_set.end());
+            }
+        }
+    }
+    return ans;
 }
 
 // Define any helper functions here
